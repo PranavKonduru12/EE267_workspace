48de867afb800d5c1c27c0484e0ca374c0f77d01
Pranav <pskonduru12+gh@gmail.com>
2025-02-21 13:14:01 -0800

    added average kernel

--- HW2_EE267.m
+++ HW2_EE267.m
@@ -16,34 +16,55 @@ noisy_guaus_image = imnoise(G, 'gaussian', 0.02);
 %Combine salt & pepper with gaussian noise
 noisy_combine_image = imnoise(noisy_salt_image, 'gaussian', 0.02);
 
-%Smoothing (Averaging) Filter
+%%Smoothing (Averaging) Filter
 
-%Median Filter
+% Convert G to double for proper convolution
+G_double = double(noisy_combine_image);
 
-%Smootinng-Median Filter
+% Define a smoothing kernel (can also use fspecial function)
+smoothing_kernel = (1/8) * [ 1 1 1; 
+                             1 1 1; 
+                             1 1 1];
+
+% Apply convolution (may need to use imfilter when using fspecial) 
+smoothing_result = conv2(G_double, smoothing_kernel, 'same');
+
+
+%%Median Filter
+
+%%Smootinng-Median Filter
 
 
 
 % Display original and noisy image
-figure;
-subplot(1,3,1);
-imshow(G);
-title('Original Image');
-
-subplot(1,3,2);
-imshow(noisy_salt_image);
-title('Image with Salt & Pepper Noise');
-
-subplot(1,3,3);
-imshow(noisy_guaus_image);
-title('Image with Gaussian Noise');
+% figure;
+% subplot(1,3,1);
+% imshow(G);
+% title('Original Image');
+% 
+% subplot(1,3,2);
+% imshow(noisy_salt_image);
+% title('Salt & Pepper Noise');
+% 
+% subplot(1,3,3);
+% imshow(noisy_guaus_image);
+% title('Gaussian Noise');
+% 
+% figure;
+% subplot(1,2,1);
+% imshow(noisy_guaus_image);
+% title('Gaussian Image');
+% 
+% subplot(1,2,2);
+% imshow(noisy_combine_image);
+% title('Salt & Pepper and Gaussian');
 
 figure;
 subplot(1,2,1);
-imshow(noisy_guaus_image);
-title('Gaussian Image');
-
-subplot(1,2,2);
 imshow(noisy_combine_image);
 title('Salt & Pepper and Gaussian');
+
+subplot(1,2,2);
+imshow(smoothing_result, []);
+title('Smooth Filter');
 
